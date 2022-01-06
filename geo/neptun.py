06538621662a788c406f5d080fc80b9b4c2b952e
T23 <petr777@yahoo.com>
2022-01-07 02:53:10 +0300

    add neptun

---
+++ geo/neptun.py
@@ -0,0 +1,42 @@
+import requests
+import re
+import pandas as pd
+import json
+import html_text
+
+def get_data():
+    params = (
+        ('mid', '10E-2JlEOdsN7qEbPWQWa9XS4DENhOB5O'),
+    )
+    response = requests.get(
+        'https://www.google.com/maps/d/viewer',
+        params = params
+    )
+    data = re.findall('var _pageData = "\[\[(.*)]]";', response.text)[0]
+    data = f'[[{data}]]'
+    data = data.replace('\\', '')
+    data = json.loads(data)
+    data = data[1][6][0][12][0][13][0]
+    good_data = []
+    for row in data:
+        item = {}
+        item['name'] = row[5][0][1][0]
+        coords = row[1][0][0]
+        item['x'], item['y'] = coords[0], coords[1]
+        fragment = row[5][1][1][0].split('+')
+        address = html_text.extract_text(fragment[0])
+        if address[-1] == 'n':
+            item['address'] = address[:-1]
+        else:
+            item['address'] = address
+        good_data.append(item)
+    return good_data
+
+
+def pd_data():
+    good_data=get_data()
+    df = pd.DataFrame(good_data)
+    df['brand_name']='Neptun'
+    df['holding_name']='Neptun'
+    return df
+
