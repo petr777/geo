5c33c239c39668f909f3858eaafef07488b74283
T23 <petr777@yahoo.com>
2022-01-10 20:49:46 +0300

    add kolomarket

---
+++ geo/kolomarket.py
@@ -0,0 +1,31 @@
+import requests
+import re
+import pandas as pd
+import json
+
+
+def get_data():
+    response = requests.get(
+        'https://kolomarket.com.ua/ru/our-shops/',
+    )
+    data = re.findall('var shopsList = \[(.*?)}]', response.text)[0]
+    data = '['+ data + '}]'
+    JSON = json.loads(data)
+    good_data = []
+    for row in JSON:
+        for shop in row['cityShopsList']:
+            item = dict()
+            item['address'] = shop[0]
+            item['lat'] = float(shop[1])
+            item['lon'] = float(shop[2])
+            item['work_time'] = f'{shop[3]} - {shop[4]}'
+            good_data.append(item)
+    return good_data
+
+def pd_data():
+    good_data = get_data()
+    df = pd.DataFrame(good_data)
+    df['brand_name'] = 'КОЛО'
+    df['holding_name'] = 'АРИТЕЙЛ'
+    return df
+
